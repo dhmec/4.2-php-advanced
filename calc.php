eb957d9d7914fb0c2c139bb8ab3788cb9fdfc0a1
dhmec <dharmoradiya@gmail.com>
2020-10-02 12:04:01 -0600

    Add Snack.Class.php

--- calc.php
+++ calc.php
@@ -1,47 +1,76 @@
 <?php
+// We need to use SESSION_START to declare to PHP
+// that we want to use the $_SESSION global var!
+// It sets up a unique identifier with the browser
+// so that the array of values is associated with
+// only the one user.
+session_start();
+
+// Make sure to set a default value, if this
+// key/value pair does not yet exist in the
+// associative SESSION array!
+// *** We can't array_push() to a NULL
+//     (undefined) value!
+if ( !isset( $_SESSION['calcHistory'] ) )
+{
+  $_SESSION['calcHistory'] = array();
+}
+
 // Try to avoid use of globals unless they are absolutely necessary...
 $GLOBALS['pageTitle'] = 'PHP Calculator';
 
 // Show our header.
 include './templates/header.php';
 
-// If we want to read values from a GET method submission...
-// we use the $_GET superglobal! It is an associative array.
+// If we want to read values from a POST method submission...
+// we use the $_POST superglobal! It is an associative array.
 echo '<pre>';
-var_dump( $_GET );
+var_dump( $_POST );
 var_dump( $_POST ); // POST is handled the same way!
 echo '</pre>';
 
 // Set result to false so we can check later if it should be output.
 $result = FALSE;
-if ( !empty( $_GET ) ) // Check if there are any values in our array!
+if ( !empty( $_POST ) ) // Check if there are any values in our array!
 { // We need to do a different math operation depending on submission...
-  switch ( $_GET['op'] )
+  switch ( $_POST['op'] )
   { // A case for each possible <option> in our form...
     case 'addition':
-      $result = $_GET['value1'] + $_GET['value2'];
+      $opSymbol = '+';
+      $result = $_POST['value1'] + $_POST['value2'];
       break;
     case 'subtraction':
-      $result = $_GET['value1'] - $_GET['value2'];
+      $opSymbol = '-';
+      $result = $_POST['value1'] - $_POST['value2'];
       break;
     case 'multiplication':
-      $result = $_GET['value1'] * $_GET['value2'];
+      $opSymbol = '&times;';
+      $result = $_POST['value1'] * $_POST['value2'];
       break;
     case 'division':
-      $result = $_GET['value1'] / $_GET['value2'];
+      $opSymbol = '&divide;';
+      $result = $_POST['value1'] / $_POST['value2'];
       break;
   }
+  // Add this result to the calculator history array!
+  array_push(
+    $_SESSION['calcHistory'],
+    "{$_POST['value1']} {$opSymbol} {$_POST['value2']} = {$result}"
+  );
 }
+echo '<pre>';
+var_dump( $_SESSION );
 // Use var_dump, much like we did in JS with console.log.
 // It outputs the data-type and value of what you pass in!
 var_dump( $result ); // What is our result, right now!?
+echo '</pre>';
 ?>
 
 <p>
   Welcome to our Calculator page!
 </p>
 
-<form method="GET" action="calc.php">
+<form method="POST" action="calc.php">
   <label for="num1">
     Enter first operand:
     <input
---
+++ includes/Snack.Class.php
@@ -0,0 +1,45 @@
+<?php
+class Snack
+{
+  /**
+   * We can set up properties like so...
+   */
+  public $name;
+  public $type;
+  public $price;
+  public $calories;
+
+  /**
+   * Constructor method runs every time we
+   * make a new instance of this class (a
+   * new object following this blueprint.)
+   */
+  function __construct ( $snackName = '', $snackType = '', $snackPrice = 0.00, $snackCalories = 0 )
+  {
+    $this->name = $snackName;
+    $this->type = $snackType;
+    // For more verbose currency formatting, check out:
+    // https://www.php.net/manual/en/numberformatter.formatcurrency.php
+    // Thanks Fahad!
+    $this->price = number_format(
+      $snackPrice, // Number to format.
+      2, // Number of decimal places.
+      '.', // Decimal separator.
+      ',' // Thousands separator. 1,000.00
+    );
+    $this->calories = ( (integer) $snackCalories ); // Convert $snackCalories to int. // Could instead use intval()
+  }
+
+  public function caramelize()
+  {
+    $this->calories *= 2; // Same as... $this->calories = $this->calories * 2;
+  }
+}
+
+// Initialize a Snack object, pass arguments to __construct.
+$mySnack = new Snack( 'Oh Henry', 'chocolate', 1.895555, "200.907" );
+var_dump( $mySnack );
+
+// Run a method from the object.
+$mySnack->caramelize();
+var_dump( $mySnack );

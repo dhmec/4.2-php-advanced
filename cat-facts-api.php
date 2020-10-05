215ba8c8a235c98dc80a889c726602bb7d2ccfbf
dhmec <dharmoradiya@gmail.com>
2020-10-05 11:08:59 -0600

    added cat fact api

--- cat-facts-api.php
+++ cat-facts-api.php
@@ -1,9 +1,30 @@
 <?php
 $GLOBALS['pageTitle'] = 'Cat Facts (External API Test)';
 include './templates/header.php';
-?>
+
+
+$dailyCatFactResponse = file_get_contents(
+ 'http://cat-fact.herokuapp.com/facts/random'
+);
+//var_dump( $dailyCatFactResponse );
+if( $dailyCatFactResponse )
+{
+    $dailyCatFact = json_decode( $dailyCatFactResponse );
+    ?>
+        <h2>Today's Cat Fact:</h2>
+        <p><?php echo $dailyCatFact->text; ?></p>
+    <?php
+
+}
 
 
 
 
-<?php include './templates/footer.php';
+
+
+
+
+
+
+
+ include './templates/footer.php'; 
